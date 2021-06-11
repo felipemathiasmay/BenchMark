606fd38b389f49f6911c82f71afddad4520d9b58
felipemathiasmay <62123260+felipemathiasmay@users.noreply.github.com>
2021-06-11 18:43:04 -0300

    Add files via upload

---
+++ BenchMark.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.systeminfo;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ *
+ * @author Felipe, Geovanna e Mateus
+ */
+public class BenchMark {
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     * @throws java.io.FileNotFoundException
+     * @throws java.lang.InterruptedException
+     */
+    public static void main(String[] args) throws IOException, FileNotFoundException, InterruptedException {
+        // TODO code application logic here
+        //input file
+        SystemaInfo.GetInfo();
+        Stages.readCSV();
+    }
+}
